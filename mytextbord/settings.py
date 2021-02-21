0273333e659e61741ef9be073b0b6f51fbd58071
mytaxboard <78686229+mytaxboard@users.noreply.github.com>
2021-02-21 21:40:39 +0530

    Update settings.py

--- mytextbord/settings.py
+++ mytextbord/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
