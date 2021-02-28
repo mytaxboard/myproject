0294a6a0177fe3fb09d12ee34bd5b5175a4f93a4
deepak9039 <61735881+deepak9039@users.noreply.github.com>
2021-02-28 12:14:22 +0530

    problem solve

--- mytextbord/settings.py
+++ mytextbord/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mytaxboard',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
