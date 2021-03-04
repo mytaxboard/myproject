0446998481f99e1e50f8421e5fe14221cc3a04af
narendra7770 <narendraa7770@gmail.com>
2021-03-04 11:59:34 +0530

    about us

--- mytextbord/settings.py
+++ mytextbord/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mytaxboard',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
